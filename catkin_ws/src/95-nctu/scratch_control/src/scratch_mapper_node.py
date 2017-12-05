5a504e7470ed44553ff266cb2a43787ff18d01df
dalek669528 <dalek669528@gmail.com>
2017-12-06 02:04:27 +0800

    scratch fsm

--- catkin_ws/src/95-nctu/scratch_control/src/scratch_mapper_node.py
+++ catkin_ws/src/95-nctu/scratch_control/src/scratch_mapper_node.py
@@ -68,8 +68,7 @@ class JoyMapper(object):
         if not self.state_scratch:
             self.publishControl()
             self.processButtons(joy_msg)
-#
-"""
+    """
     def cbScratch(self, scratch_msg):
         self.scratch_msg = scratch_msg.data
         if (self.scratch_msg == "go"):
@@ -94,8 +93,7 @@ class JoyMapper(object):
         	self.state_scratch = False
         self.publishControlForScratch()
         self.processButtons(joy_msg)
-"""
-#
+    """
     def publishControl(self):
         car_cmd_msg = Twist2DStamped()
         car_cmd_msg.header.stamp = self.joy.header.stamp
